0599effdbce611651bcbfb57805ade596d405ac2
abdu <torabekturopov1@gmail.com>
2024-08-16 10:06:57 +0500

    model va admin bn ishlandi

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import NewsModel
+from .models import Category, NewsModel
 
 # Register your models here.
-admin.site.register(NewsModel)
+# admin.site.register(Category)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(NewsModel)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish_time', 'created_time']
+    list_filter = ['title', 'created_time', 'status']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish_time'
+    search_fields = ['title', 'body']
+    ordering = ['status', 'publish_time']
--- app/models.py
+++ app/models.py
@@ -1,11 +1,50 @@
 from django.db import models
+import uuid
+
+from django.utils import timezone
+
 
 # Create your models here.
 
-class NewsModel(models.Model):
-    title = models.CharField(max_length=2000)
-    text = models.TextField()
-    images = models.ImageField()
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=NewsModel.Status.Draft)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, default='default_category')
 
     def __str__(self):
+        return self.name
+
+
+class NewsModel(models.Model):
+    # id = models.IntegerField(primary_key=True, unique=True)
+    # uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    class Status(models.TextChoices):
+        Draft = "Df", "Draft"
+        Published = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    slug = models.SlugField(max_length=250)
+    images = models.ImageField(upload_to='news/images')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publish_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.Draft)
+
+    objects = models.Manager()
+
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish_time']
+
+    def __str__(self):
+
         return self.title
